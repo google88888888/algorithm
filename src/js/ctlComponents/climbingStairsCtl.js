eff4f33ed1c40dd7d16298fd9bb7a0165faf3da4
jiuxiang meng <jxmeng@aerohive.com>
2019-05-09 18:55:54 +0800

    1

--- src/js/ctlComponents/climbingStairsCtl.js
+++ src/js/ctlComponents/climbingStairsCtl.js
@@ -18,10 +18,35 @@ class climbingStairsCtl extends Component {
     
     componentDidMount() {
         let self=this;  
-        self.setState({
-            output:"44444444444444444444rrrrrrrrrrrrrr"
-        })
+
+        var input=[-2,1,-3,4,-1,2,1,-5,4];
+
+        var a=[];
+        for(var i=0;i<input.length;i++){
+            a.push([]);
+        }
+
+        for(var i=0;i<input.length;i++){
+                a[i][i]=input[i]; 
+        }
+        var max=[0][0];
+        for(var i=0;i<input.length;i++){
+            for(var j=i+1;j<input.length;j++){
+                console.log("11111111111111111111111");
+                console.log(a[i][j-1]);
+                console.log(input[j]);
+                a[i][j]=a[i][j-1]+input[j];
+                if(a[i][j]>max){
+                    max=a[i][j];
+                }
+            }
+            
+        }
         
+
+        self.setState({
+            output:max,
+        })
     }
     render() {
         const {output} = this.state;
